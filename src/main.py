96ac0b37f5d171ddc15a589e9ccde5fdcb381e95
master172 <themasterprogrammer70000@gmail.com>
2025-09-08 18:10:58 +0530

    get familiar with fast api

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from fastapi import *
+from fastapi import FastAPI, Body, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
-items = []
+items = ["apple","orange","porridge"]
 
-class Item():
+class Item(BaseModel):
 	item:str
 
 @app.get("/")
@@ -19,7 +20,14 @@ def create_item(item:str = Body(..., embed=False)):
 	print(item)
 	return item
 
+@app.get("/items/{item_index}")
+def get_item_by_id(item_index:int)->str:
+	if item_index < len(items):
+		item = items[item_index]
+	else:
+		raise HTTPException(status_code=404,detail="Item not found")
+
 @app.post("/json")
-def print_json(content:dict = Body(..., embed=False)):
+def print_json(content:Item = Body(..., embed=False)):
 	print(content)
 	return content
